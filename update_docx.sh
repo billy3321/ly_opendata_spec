2a2ae19541d6bc781cec3840c5ac1f681fe219e9
billy3321 <billy3321@gmail.com>
2014-04-18 22:10:57 +0800

    update at 2014-04-18 22:10:57

--- update_docx.sh
+++ update_docx.sh
@@ -2,6 +2,8 @@
 
 
 wget 'https://docs.google.com/document/d/1DjnPJ1LgovVYeAyODcjUpwpJN1D-WLU4TErzFhrzbO0/export?format=docx&id=1DjnPJ1LgovVYeAyODcjUpwpJN1D-WLU4TErzFhrzbO0&token=AC4w5Vg9MP-Sy2BN8L2QqJRr_SEicXH_qg%3A1397824212594' -O '服務建議書.docx'
+wget 'https://docs.google.com/document/d/1DjnPJ1LgovVYeAyODcjUpwpJN1D-WLU4TErzFhrzbO0/export?format=txt&id=1DjnPJ1LgovVYeAyODcjUpwpJN1D-WLU4TErzFhrzbO0&token=AC4w5Vg9MP-Sy2BN8L2QqJRr_SEicXH_qg%3A1397824212594' -O '服務建議書.txt'
+
 
 sleep 1
 
